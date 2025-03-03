c97dc55f5bdd8619d25cbcd5f3e178755c6b300b
jasperho123 <jasper.ho28@student.tamdistrict.org>
2025-03-03 05:24:09 +0000

    3/2_1

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -33,6 +33,10 @@ export class Preloader extends Scene
         this.load.setPath('assets');
 
         this.load.image('logo', 'logo.png');
+        this.load.image('background', 'background.png' );
+        this.load.image('bg', 'bg.png');
+        this.load.image('guy', 'bird.png');
+        this.load.image('pipe', 'pipe.png');
     }
 
     create ()
