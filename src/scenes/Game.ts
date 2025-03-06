b76f76c1d67c106c45d77b5d1f2d86870a6216a2
jasperho123 <jasper.ho28@student.tamdistrict.org>
2025-03-06 21:53:10 +0000

    3/6_2

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -8,7 +8,7 @@ export class Game extends Scene
     cursors: Phaser.Types.Input.Keyboard.CursorKeys;
     player: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
     bg: any;
-    pipe: any;
+    pipes: any;
     constructor ()
     {
         super('Game');
@@ -17,7 +17,7 @@ export class Game extends Scene
     preload(){
 
         this.load.image('guy', 'assets/bird.png');
-        this.load.image('pipe', 'assets/pipe.png');
+        this.load.image('pipe', 'assets/doublepipe.png');
     }
 
     create ()
@@ -25,26 +25,38 @@ export class Game extends Scene
         this.cursors = this.input.keyboard!.createCursorKeys();
         
         // this.background = this.add.image(190, 300, 'bg');
-        this.bg = this.add.tileSprite(190,300, 390, 600, 'bg')
-        
-        // this.pipe = this.add.tileSprite(190, 300, 270, 148, 'pipe').setScale(2.5);
+        this.bg = this.add.tileSprite(190,300, 390, 600, 'bg');
 
         this.player = this.physics.add.image(10,10, 'guy').setScale(2.5);
         this.player.setCollideWorldBounds(true);
-
         
-
-        this.input.once('pointerdown', () => {
-            // float player
+        this.pipes = this.physics.add.group( {
+            allowGravity: false,
         });
+        
+        this.createPipe();
+
     }
 
     update() {
-            if (this.cursors.space.isDown) {
+        if (this.cursors.space.isDown) {
             this.player.setVelocityY(-150);
         }
 
         this.bg.tilePositionX += 1;
-        // this.pipe.tilePositionX += 1;
+        this.pipes.children.iterate((pipe: any) => {
+            // move left ( x-- )
+            pipe.x--;
+            if (pipe.x < 0){
+                this.pipes.remove(pipe);
+                this.createPipe();
+            }
+        })
+    }
+
+    createPipe(){
+        // Phaser.Math.FloatBetween
+        let pipe = this.add.sprite(400,400,'pipe');
+        this.pipes.add(pipe);
     }
 }
